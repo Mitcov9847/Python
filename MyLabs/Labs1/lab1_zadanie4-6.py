195aacf981dc635375fc2e7357e52e5db9dc51d4
Evgheniy <147694568+Mitcov9847@users.noreply.github.com>
2024-02-15 10:43:08 +0200

    Add files via upload

---
+++ MyLabs/Labs1/lab1_zadanie4-6.py
@@ -0,0 +1,16 @@
+
+age = 25 
+height = 1.75 
+city = "Питер" 
+
+description = """
+description - текстовое значение, 
+которое занимает несколько строк
+description
+description
+"""
+
+print(age)
+print(description)
+print(f"Длинна текстовой строки {len(description)}")
+
