8ea13de8c17193b7593cdd46b0c26cb164c5d867
Evgheniy <147694568+Mitcov9847@users.noreply.github.com>
2024-02-15 11:07:37 +0200

    Add files via upload

---
+++ MyLabs/Labs1/lab1_zadanie2_1-2com.py
@@ -0,0 +1,16 @@
+
+txt = "Florida breaking news and headlines."
+
+txt2 = "More results from text..."
+
+
+substr = txt[0:8] 
+print(substr)
+
+
+print(substr.strip()) # Удаляем пробелы из начала и конца подстроки
+
+print(txt2.split()) # Метод помогает разбить каждое слово по отельности.
+
+
+
